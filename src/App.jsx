f714553eed2cad871b3094b93ac01b789b39d7c4
salala1005 <jin.mijeong@gmail.com>
2024-02-06 23:54:43 +0000

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Carousel } from "bootstrap";
+import NavBar from "./Components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return <div>
-
+        <NavBar />
     </div>
 }
 
