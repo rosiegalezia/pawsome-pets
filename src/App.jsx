491a6ef65f9280ecb0462d3182a23831da3ef273
Liam Cottrell <lemcottrell@gmail.com>
2024-02-07 20:48:49 +0000

    start building img card

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@
 import GenerateName from './Pages/GenerateName'
 import About from './Pages/About'
 import NavBar from "./Components/NavBar";
+import ImageCard from './Components/ImageCard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return <div>
         <NavBar />
-        <About/>
+        <ImageCard />
     </div>
 }
 
