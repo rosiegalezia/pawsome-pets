b6da2490299cf65aae25ea2c3ab7eaa9afb6ec31
e-davies <emmi.lou002@gmail.com>
2024-02-07 19:08:01 +0000

    added dog fact api test file

--- dogFactAPITest.js
+++ dogFactAPITest.js
@@ -2,12 +2,14 @@
 // let queryURLWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + destination + "&units=metric&appid=6b4a10c6ed815160709463b2908e2d4d";
 
 let apiKey = "live_YfWC06FaSScnxQmCVmhGtpZkjdXWNT1MWyQyFQNwXWvkZI3Z9KVttI08TsgFY5a7"; 
-let queryURLDogFacts = "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=" + apiKey;
+// let breed = "Labrador";
+// let queryURLDogFacts = "https://api.thedogapi.com/v1/images/search?breed_ids="+ breed +"&api_key=" + apiKey;
+let queryURLDogFacts = "https://api.thedogapi.com/v1/images/search?breed_ids=41&api_key=" + apiKey; //breed_ids=41 should bring up Bernese Mountain Dog
 
 
 fetch(queryURLDogFacts)
     .then(function(response){
         return response.json();
     }).then(function (data){
-
+        console.log(data);
     });
