0a2ee36420505a4180f537a23f9503f7a56ebc49
e-davies <emmi.lou002@gmail.com>
2024-02-07 20:45:50 +0000

    created a json file to hold the list of dog breeds and ID's from the dog API

--- dogFactAPITest.js
+++ dogFactAPITest.js
@@ -1,16 +1,24 @@
+import dogs from './src/assets/dogFacts.json';
+
+
 let selectedBreed ; //what the user selects from the drop down
 let selectedBreedID ; // what the dog api breed id is based on the users selection above. 
 
-// How do I get a list of all the dog breeds and ID's from the json?
+// Need to import dogFacts.json into react when i move it over to a component/page
+// How do I get a list of all the dog breeds and ID's from the json? 
 // How do I take user input for dog breed and return dog breed ID??
 
 
-const apiKey = "live_YfWC06FaSScnxQmCVmhGtpZkjdXWNT1MWyQyFQNwXWvkZI3Z9KVttI08TsgFY5a7"; 
-let queryURLDogFacts = "https://api.thedogapi.com/v1/images/search?breed_ids=41&api_key=" + apiKey; //breed_ids=41 should bring up Bernese Mountain Dog
-// let queryURLDogFacts = "https://api.thedogapi.com/v1/images/search?breed_ids=" + selectedBreedID + "&api_key=" + apiKey; 
-
 // function to randomly generate a number to use in the dog fact API so that a random dog breed is called from the API
 const randomDogBreedID = () => console.log(Math.floor(Math.random()* 264) + 1); 
+
+
+
+/******************************** Dog Fact API call ***********************************************/
+
+const apiKey = "live_YfWC06FaSScnxQmCVmhGtpZkjdXWNT1MWyQyFQNwXWvkZI3Z9KVttI08TsgFY5a7"; 
+/*TEST API*/let queryURLDogFacts = "https://api.thedogapi.com/v1/images/search?breed_ids=41&api_key=" + apiKey; //breed_ids=41 should bring up Bernese Mountain Dog
+// let queryURLDogFacts = "https://api.thedogapi.com/v1/images/search?breed_ids=" + selectedBreedID + "&api_key=" + apiKey; 
 
 
 fetch(queryURLDogFacts)
