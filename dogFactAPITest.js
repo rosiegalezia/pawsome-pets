9aff646db6ef87964b7ca0f8bf6c8010391b6ec1
e-davies <emmi.lou002@gmail.com>
2024-02-07 19:41:37 +0000

    added variables ready for breed selection by user and conversion to breed ID and list API calls needed

--- dogFactAPITest.js
+++ dogFactAPITest.js
@@ -1,15 +1,22 @@
+let selectedBreed ; //what the user selects from the drop down
+let selectedBreedID ; // what the dog api breed id is based on the users selection above. 
 
-// let queryURLWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + destination + "&units=metric&appid=6b4a10c6ed815160709463b2908e2d4d";
-
-let apiKey = "live_YfWC06FaSScnxQmCVmhGtpZkjdXWNT1MWyQyFQNwXWvkZI3Z9KVttI08TsgFY5a7"; 
-// let breed = "Labrador";
-// let queryURLDogFacts = "https://api.thedogapi.com/v1/images/search?breed_ids="+ breed +"&api_key=" + apiKey;
+const apiKey = "live_YfWC06FaSScnxQmCVmhGtpZkjdXWNT1MWyQyFQNwXWvkZI3Z9KVttI08TsgFY5a7"; 
 let queryURLDogFacts = "https://api.thedogapi.com/v1/images/search?breed_ids=41&api_key=" + apiKey; //breed_ids=41 should bring up Bernese Mountain Dog
+// let queryURLDogFacts = "https://api.thedogapi.com/v1/images/search?breed_ids=" + selectedBreedID + "&api_key=" + apiKey; 
 
 
 fetch(queryURLDogFacts)
     .then(function(response){
         return response.json();
     }).then(function (data){
+        let dog = data[0].breeds[0];
+
         console.log(data);
+        console.log('dog id: ' + dog.id);
+        console.log('dog name: ' + dog.name);
+        console.log('breed group: ' + dog.breed_group);
+        console.log('bred for: ' + dog.bred_for);
+        console.log('life span: ' + dog.life_span);
+        console.log('temperament: ' + dog.temperament);
     });
