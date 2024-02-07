dd8a15880d1bd95238cfb4d707ca64724aa191a4
salala1005 <jin.mijeong@gmail.com>
2024-02-07 13:30:18 +0000

    carousel added, navbar path linked

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+// import '../App.css';
+import NavBar from '../Components/NavBar';
+import { Carousel } from 'bootstrap';
+import '../Components/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap'
+import CarouselFade from '../Components/Carousel';
+
+function Home() {
+  return (
+    <div>
+      <CarouselFade />
+            {/* <div className="social d-flex justify-content-center" id="contact-icon">
+            <a className="mx-3" href="https://github.com/Salala1005" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
+          </div> */}
+    </div>
+  )
+}
+
+export default Home;
