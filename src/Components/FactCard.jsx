499ecd2e97a37a66ebf1206a717d89ea18927944
rosiegalezia <rosegalezia@gmail.com>
2024-02-08 20:54:50 +0000

    closed card.body tag

--- src/Components/FactCard.jsx
+++ src/Components/FactCard.jsx
@@ -1,7 +1,7 @@
 /*********** TO DO ************/
 
 // Center the input and buttons. 
-    // Should the btns go at the end of input box or below...?
+// Should the btns go at the end of input box or below...?
 
 
 
@@ -22,9 +22,9 @@ import React, { useState, useEffect } from 'react';
 
 function FactCard() {
 
-        // Need to populate this with user choice
-        const breedName = "chihuahua";
-        
+    // Need to populate this with user choice
+    const breedName = "chihuahua";
+
     const [cardImg, setCardImg] = useState(null);
     const imgAlt = "a " + breedName;
     const queryUrlImg = "https://dog.ceo/api/breed/" + breedName + "/images/random/50";
@@ -43,48 +43,49 @@ function FactCard() {
 
     return (
         <div>
-        <Card style={{ width: '38rem' }} bg="dark" text='light' className='card m-2 text-center  mx-auto d-flex justify-content-center'>
-            <Card.Img variant="top" src="src/assets/example.jpg" style={{ objectFit: "cover"}} /> {/*Currently a placeholder dog img */}
-            <Card.Body>
-                <Card.Title className='mb-4' >Dog Breed</Card.Title>
-                
-                {/* <ListGroup className='mb-3' as="ul">
+            <Card style={{ width: '38rem' }} bg="dark" text='light' className='card m-2 text-center  mx-auto d-flex justify-content-center'>
+                <Card.Img variant="top" src="src/assets/example.jpg" style={{ objectFit: "cover" }} /> {/*Currently a placeholder dog img */}
+                <Card.Body>
+                    <Card.Title className='mb-4' >Dog Breed</Card.Title>
+
+                    {/* <ListGroup className='mb-3' as="ul">
                     <ListGroup.Item variant="dark" as="li"><span className='fw-bold'>Info 1: </span>xscvbfghjbmn</ListGroup.Item>
                     <ListGroup.Item variant="dark" as="li"><span className='fw-bold'>Info 2: </span>xscvbfghjbmn</ListGroup.Item>
                     <ListGroup.Item variant="dark" as="li" ><span className='fw-bold'>Info 3: </span>xscvbfghjbmn</ListGroup.Item>
                     <ListGroup.Item variant="dark" as="li"><span className='fw-bold'>Info 4: </span>xscvbfghjbmn</ListGroup.Item>
                 </ListGroup> */}
 
-                <Card.Body>
-                    <Card.Title className='mb-4' >{breedName}</Card.Title>
+                    <Card.Body>
+                        <Card.Title className='mb-4' >{breedName}</Card.Title>
 
-                    {/* <ListGroup className='mb-3' as="ul">
+                        {/* <ListGroup className='mb-3' as="ul">
                         <ListGroup.Item variant="dark" as="li"><span className='fw-bold'>Info 1: </span>xscvbfghjbmn</ListGroup.Item>
                         <ListGroup.Item variant="dark" as="li"><span className='fw-bold'>Info 2: </span>xscvbfghjbmn</ListGroup.Item>
                         <ListGroup.Item variant="dark" as="li" ><span className='fw-bold'>Info 3: </span>xscvbfghjbmn</ListGroup.Item>
                         <ListGroup.Item variant="dark" as="li"><span className='fw-bold'>Info 4: </span>xscvbfghjbmn</ListGroup.Item>
                     </ListGroup> */}
 
-                    <ListGroup className='mb-3' as="ul" style={{textAlign: "left"}}>
-                        <ListGroup.Item variant="light" as="li"><span className='fw-bold'>Breed group: </span>xscvbfghjbmn</ListGroup.Item>
-                        <ListGroup.Item variant="light" as="li"><span className='fw-bold'>Bred for: </span>xscvbfghjbmn</ListGroup.Item>
-                        <ListGroup.Item variant="light" as="li" ><span className='fw-bold'>Life span: </span>xscvbfghjbmn</ListGroup.Item>
-                        <ListGroup.Item variant="light" as="li"><span className='fw-bold'>Temperament: </span>xscvbfghjbmn</ListGroup.Item>
-                    </ListGroup>
+                        <ListGroup className='mb-3' as="ul" style={{ textAlign: "left" }}>
+                            <ListGroup.Item variant="light" as="li"><span className='fw-bold'>Breed group: </span>xscvbfghjbmn</ListGroup.Item>
+                            <ListGroup.Item variant="light" as="li"><span className='fw-bold'>Bred for: </span>xscvbfghjbmn</ListGroup.Item>
+                            <ListGroup.Item variant="light" as="li" ><span className='fw-bold'>Life span: </span>xscvbfghjbmn</ListGroup.Item>
+                            <ListGroup.Item variant="light" as="li"><span className='fw-bold'>Temperament: </span>xscvbfghjbmn</ListGroup.Item>
+                        </ListGroup>
 
 
-                    {/* <ListGroup className='mb-2' variant="flush">
+                        {/* <ListGroup className='mb-2' variant="flush">
                         <ListGroup.Item border="dark" ><span className='fw-bold'>Info 1: </span>xscvbfghjbmn</ListGroup.Item>
                         <ListGroup.Item className='text-bg-secondary p-3'><span className='fw-bold'>Info 2: </span>xscvbfghjbmn</ListGroup.Item>
                         <ListGroup.Item className='text-bg-secondary p-3'><span className='fw-bold'>Info 3: </span>xscvbfghjbmn</ListGroup.Item>
                     </ListGroup> */}
 
-                <Card.Text >
-                    Have you found your fur-ever friend? <br/> If so, why not get some help to chose the paw-fect name for them.
-                </Card.Text>
-                    <Button className='me-4 mb-4' variant="primary">Pick a name for your pet</Button>
-                    <Button className='me-4 mb-4' variant="primary" disabled="true">Add to favourites</Button>
-                    <Button className='mb-4' variant="primary">Get a new image</Button>
+                        <Card.Text >
+                            Have you found your fur-ever friend? <br /> If so, why not get some help to chose the paw-fect name for them.
+                        </Card.Text>
+                        <Button className='me-4 mb-4' variant="primary">Pick a name for your pet</Button>
+                        <Button className='me-4 mb-4' variant="primary" disabled="true">Add to favourites</Button>
+                        <Button className='mb-4' variant="primary">Get a new image</Button>
+                    </Card.Body>
                 </Card.Body>
             </Card>
         </div>
