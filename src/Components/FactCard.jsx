c0449183f82b9db29c55098284eb5937973e3158
Liam Cottrell <lemcottrell@gmail.com>
2024-02-08 19:36:15 +0000

    add images to factcard

--- src/Components/FactCard.jsx
+++ src/Components/FactCard.jsx
@@ -1,24 +1,28 @@
-/*********** TO DO ************/
-
-// Center Card
-// Remove Dog image and add seperate card to left for image
-// Center Dog Breed
-// Center buttons
-
-
-
 
 //Imported Components from React Bootstrap 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+// Image API stuff (work in progress)
+// let breedName = "chihuahua";
+// let imgAlt = "a " + breedName;
+// let queryUrlImg = "https://dog.ceo/api/breed/" + breedName + "/images/random/50";
+// let cardImg = "src/assets/example.jpg";
 
+// fetch(queryUrlImg)
+// .then(function (response) {
+//     return response.json();
+// }).then(function(data) {
+//     let cardImg = data.message[0];
+//     console.log(cardImg);
+// });
 
 function FactCard(props) {
     return (
-        <Card style={{ width: '38rem' }} bg="dark" text='light' className='m-2'>
-            <Card.Img variant="top" src="https://cdn2.thedogapi.com/images/B12BnxcVQ_1280.jpg" /> {/*Currently a placeholder dog img */}
+        <div>
+        <Card style={{ width: '38rem' }} bg="dark" text='light' className='m-2 text-center  mx-auto d-flex justify-content-center'>
+            <Card.Img variant="top" src="src/assets/example.jpg" style={{ objectFit: "cover"}} /> {/*Currently a placeholder dog img */}
             <Card.Body>
                 <Card.Title className='mb-4' >Dog Breed</Card.Title>
                 
@@ -29,7 +33,7 @@ function FactCard(props) {
                     <ListGroup.Item variant="dark" as="li"><span className='fw-bold'>Info 4: </span>xscvbfghjbmn</ListGroup.Item>
                 </ListGroup> */}
 
-                <ListGroup className='mb-3' as="ul">
+                <ListGroup className='mb-3' as="ul" style={{textAlign: "left"}}>
                     <ListGroup.Item variant="light" as="li"><span className='fw-bold'>Breed group: </span>xscvbfghjbmn</ListGroup.Item>
                     <ListGroup.Item variant="light" as="li"><span className='fw-bold'>Bred for: </span>xscvbfghjbmn</ListGroup.Item>
                     <ListGroup.Item variant="light" as="li" ><span className='fw-bold'>Life span: </span>xscvbfghjbmn</ListGroup.Item>
@@ -46,10 +50,12 @@ function FactCard(props) {
                 <Card.Text >
                     Have you found your fur-ever friend? If so, why not get some help to chose the paw-fect name for them.
                 </Card.Text>
-                <Button className='me-4' variant="primary">Pick a name for your pet</Button>
-                <Button variant="primary">Add to favourites</Button>
+                <Button className='me-4 mb-4' variant="primary">Pick a name for your pet</Button>
+                <Button className='me-4 mb-4' variant="primary">Add to favourites</Button>
+                <Button className='mb-4' variant="primary">Get a new image</Button>
             </Card.Body>
         </Card>
+        </div>
     );
 };
 
