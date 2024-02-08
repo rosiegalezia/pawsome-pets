ccfd836e8e1e8e516e811963db7083b3564c0ca2
e-davies <emmi.lou002@gmail.com>
2024-02-08 19:59:46 +0000

    added a random btn to choose a breed

--- src/Pages/Info.jsx
+++ src/Pages/Info.jsx
@@ -26,12 +26,14 @@ function Info() {
                         <Form.Group className="mb-3">
                             <Form.Label htmlFor="disabledSelect">Select your breed</Form.Label>
                             <Form.Select id="disabledSelect">
+                                <option id="breed-select">Please select a breed</option>
                                 {dogBreeds.map((breed) => {
                                     return <option id={breed.id}>{breed.breed}</option>
                                 })};
                             </Form.Select>
                         </Form.Group>
                         <Button type="submit">Show Info</Button>
+                        <Button type="random-breed" className="ms-2">Pick a random breed</Button>
                     </fieldset>
                 </Form>
 
