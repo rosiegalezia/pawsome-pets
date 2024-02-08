1e26ae288713a778a5f08bf453af6514dd06f09f
salala1005 <jin.mijeong@gmail.com>
2024-02-08 20:37:24 +0000

    navbar toggle added

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,28 +1,31 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink } from "react-router-dom";
 import "@fontsource/poppins"; // Defaults to weight 400
 import "@fontsource/poppins/400.css"; // Specify weight
 import "@fontsource/poppins/400-italic.css"; // Specify weight and style
 import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaw } from '@fortawesome/free-solid-svg-icons'
+import Logo from "../assets/logo-one-line.svg"
 
 
 function NavBar() {
   return (
-    <Navbar bg="light" data-bs-theme="light">
-    <Container>
-      <Navbar.Brand href="/#" className='logo'><FontAwesomeIcon icon={faPaw} />PAWSOME PETS</Navbar.Brand>
-      <Nav className="ms-auto">
-        <Nav.Link href="/#">Home</Nav.Link>
-        <Nav.Link href="#Info">Dog Breeds Info</Nav.Link>
-        <Nav.Link href="#GenerateName">Name Generator</Nav.Link>
-        <Nav.Link href="#About">About</Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+      <img src={Logo} alt="" className='navbar-logo'/>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/#">Home</Nav.Link>
+            <Nav.Link href="#Info">Dog Breeds Info</Nav.Link>
+            <Nav.Link href="#GenerateName">Name Generator</Nav.Link>
+            <Nav.Link href="#About">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 export default NavBar;
