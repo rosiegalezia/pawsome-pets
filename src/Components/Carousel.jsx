549f31c1295508b947e656a9c8437d37be015447
salala1005 <jin.mijeong@gmail.com>
2024-02-10 18:11:10 +0000

    navbar space wider and cat img added

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -4,7 +4,7 @@ import carouselImg2 from '../assets/carouselImg2.jpg';
 import carouselImg3 from '../assets/carouselImg3.jpg';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
-import "@fontsource/poppins"; // Defaults to weight 400
+import "@fontsource/poppins";
 import './NavBar.css';
 
 function CarouselFade() {
@@ -21,26 +21,32 @@ function CarouselFade() {
             </Button>
           </Link>
         </div>
-          <p className='carouselWelcome'>Welcome to PAWESOME PETS</p>
+        <p className='carouselWelcome'>Welcome to PAWESOME PETS</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className='c-item'>
         <img src={carouselImg3} alt='dog with stick' className='carousel-img d-block w-100' text="Second slide"/>
         <Carousel.Caption>
         <div className="mb-2">
-        <Button variant="outline-light" size="lg" className="carouselBtn">
-          Get Started
-        </Button></div>
-          <p className='carouselP'>Find your fur-ever friend and come up with a paw-fect name for them 🐾</p>
+          <Link to="Info">
+            <Button variant="outline-light" size="lg" className="carouselBtn">
+              Get Started
+            </Button>
+          </Link>
+        </div>
+        <p className='carouselP'>Find your fur-ever friend and come up with a paw-fect name for them 🐾</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className='c-item'>
-        <img src={carouselImg2} alt='dog in the beach' className='carousel-img d-block w-100' text="Third slide" />
+        <img src={carouselImg2} alt='cat walk' className='carousel-img d-block w-100' text="Third slide" />
         <Carousel.Caption>
         <div className="mb-2">
-        <Button variant="outline-light" size="lg" className="carouselBtn">
-          Get Started
-        </Button></div>
+          <Link to="Info">
+            <Button variant="outline-light" size="lg" className="carouselBtn">
+              Get Started
+            </Button>
+          </Link>
+        </div>
           <p className='carouselP'>
           </p>
         </Carousel.Caption>
