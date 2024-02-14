fc7f0ae8366e539d424f93f3261662a497533553
Liam Cottrell <lemcottrell@gmail.com>
2024-02-14 21:10:06 +0000

    tidy comments

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -8,6 +8,7 @@ function About() {
             <div className="page-content">
                 <h2 className='p-3 m-3 pt-5 text-center'>This project was created by:</h2>
                 <div className='row gx-0 m-4 d-flex justify-content-center'>
+                    {/* display card for each team member with their details */}
                     {team.map((teammember) => {
                         return <TeamMember key={teammember.id} name={teammember.name} github={teammember.github} image={teammember.image} role={teammember.role} favpet={teammember.favpet} />;
                     })}
